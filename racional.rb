6c9ed7d6a413367e7630bfa7218a957493c9dac4
ARMAS TORRES <alu4368@cc118.(none)>
2013-10-18 10:59:42 +0100

    Creado el metodo multiplicacion

--- racional.rb
+++ racional.rb
@@ -6,6 +6,8 @@ require "./gcd.rb"
 
 class Fraccion
 
+	attr_reader :num, :den
+
 	def initialize (num, den)
 		@num, @den = num, den	#Definimos las variables
 	end
@@ -14,7 +16,18 @@ class Fraccion
 		"#{@num}/#{@den}"	#Muestra numerador/denominador
 	end
 
+	def * (fraccion)
+		Fraccion.new(@num*fraccion.num, @den*fraccion.den)
+	end
+
 end
 
-racional1 = Fraccion.new(1,3)
+racional1 = Fraccion.new(1,3)		#Crea objeto
 puts "#{racional1}"
+
+racional2 = Fraccion.new (2,6)		#Multiplicacion
+puts "#{racional2}"
+racional3 = racional1*racional2
+puts "#{racional3}"
+
+
