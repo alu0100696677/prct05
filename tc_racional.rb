1504f76a9c1ed57dce20968a019a0ab29d940925
ARMAS TORRES <alu4368@cc118.(none)>
2013-10-18 11:53:36 +0100

    Añadimos el primer test para comprobar que la creacion de variables funciona bien

--- tc_racional.rb
+++ tc_racional.rb
@@ -1,8 +1,15 @@
+#!/usr/bin/ruby
+
 # Implementar en este fichero las Pruebas Unitarias asociadas a la clase Fraccion
 
 require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+
+	def test_simple
+		assert_equal("1/3", Fraccion.new(1,3).to_s)
+	end
+
 
 end
