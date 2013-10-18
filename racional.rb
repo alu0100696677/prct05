d81a37545b0332789001437fdbd6fc52415e09e5
ARMAS TORRES <alu4368@cc118.(none)>
2013-10-18 10:47:49 +0100

    Creada clase fraccion

--- racional.rb
+++ racional.rb
@@ -1,6 +1,20 @@
+#!/usr/bin/ruby
+
 # Implementar en este fichero la clase para crear objetos racionales
 
 require "./gcd.rb"
 
 class Fraccion
+
+	def initialize (num, den)
+		@num, @den = num, den	#Definimos las variables
+	end
+
+	def to_s
+		"#{@num}/#{@den}"	#Muestra numerador/denominador
+	end
+
 end
+
+racional1 = Fraccion.new(1,3)
+puts "#{racional1}"
